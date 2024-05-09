0e958eaba531d9e626851c33e0567ce3667a674c
Sebastian Arbelaez <sebastian_arbelaez@epam.com>
2024-05-09 15:54:24 +0000

    Upload New File

---
+++ src/main/java/com/example/BookStore/repository/UsersRepository.java
@@ -0,0 +1,9 @@
+package com.example.BookStore.repository;
+
+import com.example.BookStore.models.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsersRepository extends JpaRepository<Users, Long> {
+}
